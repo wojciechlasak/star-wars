6035ddfc793bdadd8c97eb92a991f70cbd5f1b76
wojciechlasak <wojciech.lasak@student.uj.edu.pl>
2021-03-29 01:27:06 +0200

    added Types for states, styled checked all button

--- src/app/films/compontents/FilmsFilter.tsx
+++ src/app/films/compontents/FilmsFilter.tsx
@@ -15,9 +15,9 @@ export interface FilmsCheckboxes {
   title: string,
 }
 
-export const FilmsFilter: React.FC<FilmsFilterProps> = ({filmsList, isShow, onChangeCheckbox}) => {
-  const [isAll, setIsAll] = useState(true)
-  const [films, setFilms] = useState(filmsList.map((film, index)  => (
+export const FilmsFilter: React.FC<FilmsFilterProps> = ({ filmsList, isShow, onChangeCheckbox }) => {
+  const [isAll, setIsAll] = useState<boolean>(true)
+  const [films, setFilms] = useState<FilmsCheckboxes[]>(filmsList.map((film, index) => (
     {
       id: index,
       isChecked: true,
@@ -37,9 +37,9 @@ export const FilmsFilter: React.FC<FilmsFilterProps> = ({filmsList, isShow, onCh
   const handeOnChangeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsAll(false);
     setFilms(films.map(film => {
-      if (e.target.value === film.title) 
+      if (e.target.value === film.title)
         film.isChecked = e.target.checked;
-      
+
       return film;
     }));
     onChangeCheckbox(films);
@@ -47,14 +47,14 @@ export const FilmsFilter: React.FC<FilmsFilterProps> = ({filmsList, isShow, onCh
 
   return (
     <>
-    {isShow && (
-    <div className="films-filter-container">
-      <div onClick={handleClickAll}>{isAll ? 'Wyczyść' : "Zaznacz wszystkie"}</div>
-      {films.map((film) => (
-        <FilmCheckbox key={film.title} film={film} onChange={handeOnChangeCheckbox}/>
-      ))}
-    </div>
-    )}
+      {isShow && (
+        <div className="films-filter-container">
+          <div className="films-filter-checked-all" onClick={handleClickAll}>{isAll ? 'Clear' : "Select all"}</div>
+          {films.map((film) => (
+            <FilmCheckbox key={film.title} film={film} onChange={handeOnChangeCheckbox} />
+          ))}
+        </div>
+      )}
     </>
   )
 } 
