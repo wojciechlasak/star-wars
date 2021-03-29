9ad601bbb938e6e52795407fe127a7125ad5a7b5
wojciechlasak <wojciech.lasak@outlook.com>
2021-03-29 19:06:20 +0200

    changed tsx to tx,

--- src/app/characters/characktersActions.ts
+++ src/app/characters/characktersActions.ts
@@ -17,7 +17,6 @@ export const fetchCharacters = (url: string) => async (dispatch: Dispatch<Charac
       type: CHARACTERS_LOADING
     })
 
-
     const firstPageData = await fetch(url)
     const firstPageDataJson = await firstPageData.json();
     const numberOfPages  = await firstPageDataJson.count;
